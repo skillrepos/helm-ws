8fb384706efb2568d660d3e153fe734039d13da3
Brent Laster <bcl@nclasters.org>
2022-06-20 22:30:22 -0400

    make script more flexible

--- extra/roar-port.sh
+++ extra/roar-port.sh
@@ -1,3 +1,3 @@
- export POD_NAME=$(kubectl get pods --namespace roar -l "app=roar-web" -o jsonpath="{.items[0].metadata.name}")
-  kubectl port-forward $POD_NAME $1:8080 --namespace roar
+ export POD_NAME=$(kubectl get pods --namespace $1 -l "app=roar-web" -o jsonpath="{.items[0].metadata.name}")
+  kubectl port-forward $POD_NAME $2:8080 --namespace $1
 
