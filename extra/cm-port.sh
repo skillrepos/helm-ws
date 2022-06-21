c7689dec00a9f8768f5faca101529eaf8ba589f5
Brent Laster <bcl@nclasters.org>
2022-06-20 21:59:21 -0400

    update script

--- extra/cm-port.sh
+++ extra/cm-port.sh
@@ -1,4 +1,4 @@
  export POD_NAME=$(kubectl get pods --namespace default -l "app.kubernetes.io/name=chartmuseum" -o jsonpath="{.items[0].metadata.name}")
-  echo http://127.0.0.1:8080/
+
   kubectl port-forward $POD_NAME $1:8080 --namespace default
 
